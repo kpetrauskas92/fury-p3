f738952aa65684e04b369435b2d101d970220bdb
Karolis <k.petrauskas92@gmail.com>
2023-03-23 21:45:09 +0000

    Add main menu function with logo

--- run.py
+++ run.py
@@ -1,22 +1,33 @@
 """
-Googlesheets API.
+Import
 """
-# import gspread
-# from google.oauth2.service_account import Credentials
+import gspread
+from google.oauth2.service_account import Credentials
+# from game import play_game
+from art import LOGO
 
 
-# # Google Sheets API credentials
-# SCOPE = [
-#     "https://www.googleapis.com/auth/spreadsheets",
-#     "https://www.googleapis.com/auth/drive.file",
-#     "https://www.googleapis.com/auth/drive"
-# ]
-# CREDS = Credentials.from_service_account_file('creds.json')
-# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-# SHEET = GSPREAD_CLIENT.open('fury_data')
+# Google Sheets API credentials
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('fury_data')
+HIGHSCORES = SHEET.worksheet('highscores')
 
-import game
+
+def display_main_menu():
+    """ Displays main menu"""
+    print("Main Menu:\n")
+    print("1. Login")
+    print("2. Register")
+    print("3. Leaderboard")
+
 
 if __name__ == "__main__":
-    game.play_game()
+    print(LOGO)
+    display_main_menu()
