f3866dd06a845b55200c1f431131128c4418701d
Karolis <k.petrauskas92@gmail.com>
2023-03-24 17:18:59 +0000

    Update styling on art and main_menu

--- modules/main_menu.py
+++ modules/main_menu.py
@@ -1,10 +1,13 @@
 """ Main menu module """
+from colorama import Fore, Style, init
 from art import MAIN_MENU
+
+init()
 
 
 def display_main_menu():
     """Displays main menu"""
     print(MAIN_MENU)
-    print("1. Login")
-    print("2. Register")
-    print("3. Leaderboard\n")
+    print(f"{Fore.CYAN}1.{Style.RESET_ALL} Login")
+    print(f"{Fore.CYAN}2.{Style.RESET_ALL} Register")
+    print(f"{Fore.CYAN}3.{Style.RESET_ALL} Leaderboard\n")
