88f4e2723d3975187e42cab1203de4a342d8cb8c
Karolis <k.petrauskas92@gmail.com>
2023-03-26 19:15:38 +0000

    Terminal layout testing

--- controllers/default.js
+++ controllers/default.js
@@ -18,7 +18,7 @@ function socket() {
         // Spawn terminal
         client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
-            cols: 80,
+            cols: 60,
             rows: 48,
             cwd: process.env.PWD,
             env: process.env
