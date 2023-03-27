67c2ffcb6ad2b9b8e0f04b37257ccea7f4420c9d
Karolis <k.petrauskas92@gmail.com>
2023-03-27 00:34:33 +0000

    Update leader_brd styling

--- modules/leaderboard.py
+++ modules/leaderboard.py
@@ -14,14 +14,14 @@ def show_leaderboard():
     sorted_rows = sorted(rows, key=lambda row: int(row[2]), reverse=True)
 
     print(f"{Fore.GREEN}Leaderboard:{Style.RESET_ALL}")
-    print(f"{Fore.YELLOW}{'Rank:':<5}{Style.RESET_ALL}"
-          f"{Fore.YELLOW}{' Name:':<20}{Style.RESET_ALL}"
-          f"{Fore.YELLOW}{'  City:':<20}{Style.RESET_ALL}"
-          f"{Fore.YELLOW}{'   Score:':<10}{Style.RESET_ALL}")
-    print("-" * 55)
+    print(f"{Fore.YELLOW}{'Rank:':<7}{Style.RESET_ALL}"
+          f"{Fore.YELLOW}{' Name:':<15}{Style.RESET_ALL}"
+          f"{Fore.YELLOW}{'  City:':<18}{Style.RESET_ALL}"
+          f"{Fore.YELLOW}{'   Score:':<7}{Style.RESET_ALL}")
+    print("-" * 50)
 
     for i, row in enumerate(sorted_rows[:15]):
         name, city, score = row
-        print(f"{i + 1:<5} {name:<20} {city:<20} {score:<10}")
+        print(f"{i + 1:<7} {name:<15} {city:<18} {score:<7}")
 
-    input("\nPress Enter to go back to the main menu: ")
+    input("\nPress ENTER to go back to the main menu: ")
